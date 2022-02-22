65f72b17ce021ac69669a27b07f146dcaa3dab62
Caner Ağkaya <dev.caneragkaya@gmail.com>
2022-02-22 19:32:51 +0300

    Initial Commit

---
+++ FaceIdLogin/FaceIdLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceIdLoginApp.swift
+//  FaceIdLogin
+//
+//  Created by Caner Ağkaya on 22.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct FaceIdLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
